af58194663e46662f55d6bd2472566b4d01fbda9
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2020-01-23 12:04:02 +0700

    Update header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
+// Angular module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Component import
 import { StudentsComponent } from './core/students/students.component';
 import { ClassComponent } from './core/class/class.component';
 import { InstructorComponent } from './core/instructor/instructor.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
     ClassComponent,
     InstructorComponent,
     StudentsComponent
