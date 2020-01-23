e889ae95ec2b4fd8cdbf505c66e7b00dc9fe49e9
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2020-01-23 11:54:05 +0700

    Add component class, student and instructor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './core/students/students.component';
+import { ClassComponent } from './core/class/class.component';
+import { InstructorComponent } from './core/instructor/instructor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StudentsComponent,
+  },
+  {
+    path: 'class',
+    component: ClassComponent
+  },
+  {
+    path: 'instructor',
+    component: InstructorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './core/students/students.component';
+import { ClassComponent } from './core/class/class.component';
+import { InstructorComponent } from './core/instructor/instructor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassComponent,
+    InstructorComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
